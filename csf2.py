0c6422c4242d9b494ce344099e7cb507562f0325
Chimi021 <02240116.cst@rub.edu.bt>
2025-03-01 22:58:37 +0600

    lil fun?

---
+++ csf2.py
@@ -0,0 +1,19 @@
+name = ("chhimi")
+condition = True
+print ( "My name is " + name )
+print (condition)
+
+a = 10
+b = 20
+c = a * b 
+print (c)
+
+PunakhaT = 15
+ThimphuT = 10
+if PunakhaT > ThimphuT: 
+    print ("Punakha is hotter then Thimphu")
+else:
+    print ("Thimphu is hotter then punakha")
+
+
+
---
+++ trial.py
@@ -0,0 +1,2 @@
+print ("Hello")
+print ("What the hell m i actually doing .. now even more confused :(((")
