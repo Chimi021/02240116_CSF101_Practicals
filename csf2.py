98a465352a300462d6b1c3d636f3c529cc34b182
Chimi021 <02240116.cst@rub.edu.bt>
2025-03-03 01:26:36 +0600

    Adding previous notes from class

--- csf2.py
+++ csf2.py
@@ -1,3 +1,44 @@
+
+#Data types and variables
+# Assignments and re assignment
+# Conditional statements and comparative operators
+# arrays
+# String manipulation
+# Passing variables
+# Functions and scope and passing parameters
+
+# Assignment
+X = 2
+Name = "Dorji Tshomo"
+Number = 1.00
+Condition = True
+# Re-assignment
+x = Condition
+print (x)
+# the most reassignment will take affect. or we can assign as many thing sas possible to a variable.
+
+#CONDITIONAL VALUES
+# <,>, <=, >= greater to or equal to, != (), == (equal to ) triple equal to === (also checks data types)
+# a single = is used for assignment and == means equal to mathematic equal
+pmoney = 1000
+fee = 2000
+taxi = 50
+snack = 300
+total = fee + snack + taxi
+if pmoney > total:
+    print ("Can")
+else:
+    print ("cannot")
+
+#if Total > fee:
+#    print ("sad ?")
+#elif snack > taxi:
+#   print ("leave snack")
+# else:
+#   print ("save money")
+
+
+
 name = ("chhimi")
 condition = True
 print ( "My name is " + name )
